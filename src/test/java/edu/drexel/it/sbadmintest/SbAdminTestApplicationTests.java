3530dd962cc7bee84d9f9796a9cb187ccc676f23
Travis Collins <tec25@drexel.edu>
2023-07-21 10:32:03 -0400

    initial commit

---
+++ src/test/java/edu/drexel/it/sbadmintest/SbAdminTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.drexel.it.sbadmintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAdminTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
